fee9b4e4ad35de4589329990037f2558cc9db97e
jeanriffel <jeanriffel1@gmail.com>
2017-10-23 20:43:13 -0200

    Including Files

---
+++ reading_file_text/readingfiletext.py
@@ -0,0 +1,15 @@
+#!/usr/bin python3
+
+def loadfile():
+    fileIn = open("file.txt", "r")
+    lines = fileIn.readlines()
+    fileIn.close()    
+    return lines
+
+if __name__ == '__main__':
+    print ("Load file ...\n")
+    
+    lines = loadfile()
+    
+    print (lines)
+    print ("\nLoad End\n")
